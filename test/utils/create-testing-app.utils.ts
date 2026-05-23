cd2c747a477dc954f8965f9d50f41501031167a1
mahesgya <anargya115@gmail.com>
2026-05-23 19:52:08 +0700

    chore(test): add user auth module to testing app

--- test/utils/create-testing-app.utils.ts
+++ test/utils/create-testing-app.utils.ts
@@ -8,18 +8,11 @@ import app from 'src/infrastructure/config/app/app.config';
 import { dataSourceOptions } from 'src/infrastructure/config/database/typeorm.config';
 import environmentValidation from 'src/infrastructure/config/environment.validation';
 import { createPinoLoggerOptions } from 'src/infrastructure/core/logger/pino-logger.factory';
+import { UserAuthModule } from 'src/features/user-auth/user-auth.module';
 import { App } from 'supertest/types';
 
 const env: string = process.env.NODE_ENV || 'test';
 
-/**
- * Creates and initializes a Nest application for testing purposes.
- * Uses a unique database per test file for isolation.
- *
- * @async
- * @param databaseName - The name of the test database to use
- * @returns {Promise<INestApplication<App>>} A Promise that resolves to the initialized Nest application.
- */
 export default async function createTestingApp(
   databaseName: string,
 ): Promise<INestApplication<App>> {
@@ -45,6 +38,8 @@ export default async function createTestingApp(
       }),
 
       TypeOrmModule.forRoot(testDataSourceOptions),
+
+      UserAuthModule,
     ],
   }).compile();
 
